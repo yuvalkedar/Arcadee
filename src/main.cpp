3bc400bfac5d95ec78e3fde9720524a0295f1a67
yuvalkedar <yuvalkedar4@gmail.com>
2019-11-25 09:16:45 +0200

    deleted classes

--- src/main.cpp
+++ src/main.cpp
@@ -14,21 +14,21 @@
 #include <Button.h>  // https://github.com/madleech/Button
 #include <timer.h>   // https://github.com/brunocalou/Timer
 #include <timerManager.h>
-#include "claweeServo.h"
+#include <Servo.h>
 
-#define LAUNCHER_BTN_PIN (2)  // Right pin in the RPi (GPIO18)
+#define LAUNCHER_BTN_PIN (4)  // Right pin in the RPi (GPIO18)
 #define LAUNCHER_PIN (3)
-#define AIMING_BTN_PIN (4)  // Front pin in the RPi (GPIO17)
+#define AIMING_BTN_PIN (2)  // Front pin in the RPi (GPIO17)
 #define AIMING_SERVO_PIN (5)
 #define MAGAZINE_SERVO_PIN (6)
-#define WINNING_PIN (7)           // winning switch pin in the RPi (GPIO12)
+#define WINNING_SENSOR_PIN (7)    // winning switch pin in the RPi (GPIO12)
 #define START_GAME_PIN (8)        // coin switch pin in the RPi (GPIO25)
 #define LIMIT_SWITCH_2_PIN (12)   // limit switch r/l pin in the RPi (GPIO20)
 #define LIMIT_SWITCH_1_PIN (13)   // limit switch f/b pin in the RPi (GPIO16)
 #define LED_BAR_PIN (A0)          // D14
 #define MAGAZINE_SENSOR_PIN (A1)  // D15
 #define BASKET_SENSOR_PIN (A2)    // D16
-#define MAGAZINE_MOTOR_PIN (A5)   // D19 = air blower
+#define MAGAZINE_BLOWER_PIN (A5)   // D19 = air blower
 
 #define NUM_PIXELS (8)
 #define GAME_TIME (20000)  // in milliseconds
@@ -37,12 +37,16 @@
 #define LAUNCHER_DELAY_MS (500)
 #define RESET_DELAY_MS (1500)
 #define LED_BAR_BRIGHTNESS (250)
-#define SERVO_UPDATE_MS (40)
 #define CANON_MIN (0)
 #define CANON_MAX (180)
 #define CALIBRATION_MS (1000)
 #define CANON_STRENGTH (49)
-#define UPDATE_MS (300)
+#define LED_BAR_UPDATE_MS (300)
+#define AIMING_UPDATE_MS (40)
+#define AIMING_SERVO_MIN (87)
+#define AIMING_SERVO_MAX (161)
+#define MAGAZINE_LOADING_POSITION (90)
+#define MAGAZINE_RESTART_POSITION (180)
 
 Button start_btn(START_GAME_PIN);
 Button aiming_btn(AIMING_BTN_PIN);
@@ -51,17 +55,17 @@ Button launcher_btn(LAUNCHER_BTN_PIN);
 Timer launcher_timer;  //a "delay" after releasing the second button and before shooting (for canon motor to reach its speed)
 Timer reset_timer;     //resets canon after shooting a ball
 Timer strength_timer;  //updates the canon's strength and led_bar
+Timer aiming_timer;  //updates the canon's position
 
-ClaweeServo aiming_servo(SERVO_UPDATE_MS);
-ClaweeServo magazine_servo(SERVO_UPDATE_MS);  // The time interval here doesn't really matter
 Adafruit_NeoPixel strength_bar(NUM_PIXELS, LED_BAR_PIN, NEO_GRB + NEO_KHZ800);
 Servo ESC;
+Servo magazine;
+Servo aiming;
 
 volatile uint8_t led_bar = 0;
 volatile uint32_t led_bar_colour[NUM_PIXELS] = {0x00cc00, 0x00cc00, 0x66cc00, 0xcccc00, 0xff9900, 0xff6600, 0xff3300, 0xff0000};
-// uint8_t increment = 1; // Only if it ever going to be different than 1?
-// uint8_t strength = CANON_MIN; // No need for it, it seems
-uint32_t test = 0x2B;
+
+volatile uint8_t position;
 
 void limit_switches(bool state) {
     digitalWrite(LIMIT_SWITCH_1_PIN, state);
@@ -70,26 +74,28 @@ void limit_switches(bool state) {
 
 void winning_check() {
     if (analogRead(BASKET_SENSOR_PIN) > BASKET_SENSOR_LIMIT) {
-        digitalWrite(WINNING_PIN, HIGH);
+        digitalWrite(WINNING_SENSOR_PIN, HIGH);
         // Serial.println(F("Congrats son, you might be the next Kobe Bryant!"));
     } else {
-        digitalWrite(WINNING_PIN, LOW);
+        digitalWrite(WINNING_SENSOR_PIN, LOW);
         // Serial.println(F("FUCKKK"));
     }
 }
 
 void game_start() {  // resets all parameters
     limit_switches(0);
-    aiming_servo.Restart();
-    magazine_servo.Magazine_restart();
+    position = AIMING_SERVO_MAX;
+    aiming.write(position);  // restart aiming servo position
+    magazine.write(MAGAZINE_RESTART_POSITION);
     ESC.write(CANON_MIN);
     led_bar = 0;
-    digitalWrite(WINNING_PIN, LOW);
+    digitalWrite(WINNING_SENSOR_PIN, LOW);
 }
 
 void reset_cb() {
-    aiming_servo.Restart();
-    magazine_servo.Magazine_restart();
+    position = AIMING_SERVO_MAX;
+    aiming.write(position);  // restart aiming servo position
+    magazine.write(MAGAZINE_RESTART_POSITION);
     ESC.write(CANON_MIN);
     led_bar = 0;
     strength_bar.clear();
@@ -97,7 +103,7 @@ void reset_cb() {
 }
 
 void launcher_cb() {  // when launcher button released
-    magazine_servo.Load();
+    magazine.write(MAGAZINE_LOADING_POSITION);
     reset_timer.start();
 }
 
@@ -131,26 +137,16 @@ void canon_update() {
     strength_bar.setPixelColor(led_bar, led_bar_colour[led_bar]);
     strength_bar.show();
 
-    // led_bar += strength;  // If it will always be 1, stick with ++led_bar below
-
     if (++led_bar >= NUM_PIXELS) {
         led_bar = NUM_PIXELS - 1;
     }
-    // INFO: Why were you checking for <= 0, it is an uint8_t! getting below zero there ... would have been a cause of the issue
 
-    // NOTICE:  Why are you doing this? when are actually turning on a led_bar + 1 ?
-    // No need for it as I see it ... but I might be wrong
-    // strength_bar.setPixelColor(led_bar + 1, 0x00);
-
-    // Serial.print("led bar: ");
-    // Serial.print(led_bar);
-
-    // Why this weird code style?
-    // (led_bar <= 5) ? strength = CANON_STRENGTH : strength = CANON_STRENGTH + 1;
-    // INFO: Only two power modes? if yes, no need for the variable
     ESC.write((led_bar <= 5) ? CANON_STRENGTH : CANON_STRENGTH + 1);
-    // Serial.print(" Canon strength: ");
-    // Serial.println(strength);
+}
+
+void aiming_update() {
+    if (--position <= AIMING_SERVO_MIN) position = AIMING_SERVO_MIN + 1;
+    aiming.write(position);
 }
 
 void setup() {
@@ -161,6 +157,11 @@ void setup() {
     aiming_btn.begin();
     strength_bar.begin();
 
+    aiming.attach(AIMING_SERVO_PIN);
+    aiming.write(AIMING_SERVO_MAX);  // restart aiming servo position
+    magazine.attach(MAGAZINE_SERVO_PIN);
+    magazine.write(MAGAZINE_RESTART_POSITION);
+
     launcher_timer.setCallback(launcher_cb);
     launcher_timer.setTimeout(LAUNCHER_DELAY_MS);
 
@@ -168,25 +169,26 @@ void setup() {
     reset_timer.setTimeout(RESET_DELAY_MS);
 
     strength_timer.setCallback(canon_update);
-    strength_timer.setInterval(UPDATE_MS);
+    strength_timer.setInterval(LED_BAR_UPDATE_MS);
+    
+    aiming_timer.setCallback(aiming_update);
+    aiming_timer.setInterval(AIMING_UPDATE_MS);
 
-    aiming_servo.Attach(AIMING_SERVO_PIN);
-    aiming_servo.Restart();
-    magazine_servo.Attach(MAGAZINE_SERVO_PIN);
-    magazine_servo.Magazine_restart();
+
 
     pinMode(LAUNCHER_BTN_PIN, INPUT);
     pinMode(AIMING_BTN_PIN, INPUT);
     pinMode(START_GAME_PIN, INPUT);
-    pinMode(WINNING_PIN, INPUT);
     pinMode(BASKET_SENSOR_PIN, INPUT);
     pinMode(MAGAZINE_SENSOR_PIN, INPUT);
+    pinMode(MAGAZINE_BLOWER_PIN, OUTPUT);
+    pinMode(WINNING_SENSOR_PIN, OUTPUT);
+    digitalWrite(WINNING_SENSOR_PIN, LOW);
 
     pinMode(LIMIT_SWITCH_1_PIN, OUTPUT);
     pinMode(LIMIT_SWITCH_2_PIN, OUTPUT);
     limit_switches(0);
 
-    pinMode(MAGAZINE_MOTOR_PIN, OUTPUT);
 
     Serial.println(F(
         "_______________________________\n"
@@ -199,20 +201,21 @@ void setup() {
 }
 
 void loop() {
-    if (check_ball_loaded()) {
+    // if (check_ball_loaded()) {
         if (start_btn.pressed()) game_start();  //based on 1000us of the coin pin
-    }
+    // }
     if (aiming_btn.pressed() || launcher_btn.pressed()) limit_switches(1);
 
-    if (!digitalRead(AIMING_BTN_PIN)) aiming_servo.Update();
-    if (!digitalRead(LAUNCHER_BTN_PIN) && !strength_timer.isRunning()) strength_timer.start();
+    if (!digitalRead(AIMING_BTN_PIN) && !aiming_timer.isRunning()) aiming_timer.start();
+    if (aiming_btn.released() && aiming_timer.isRunning()) aiming_timer.stop();
 
+    if (!digitalRead(LAUNCHER_BTN_PIN) && !strength_timer.isRunning()) strength_timer.start();
     if (launcher_btn.released() && strength_timer.isRunning()) {
         launcher_timer.start();
         strength_timer.stop();
     }
 
-    (analogRead(MAGAZINE_SENSOR_PIN) <= MAGAZINE_SENSOR_LIMIT) ? digitalWrite(MAGAZINE_MOTOR_PIN, LOW) : digitalWrite(MAGAZINE_MOTOR_PIN, HIGH);
+    (analogRead(MAGAZINE_SENSOR_PIN) <= MAGAZINE_SENSOR_LIMIT) ? digitalWrite(MAGAZINE_BLOWER_PIN, LOW) : digitalWrite(MAGAZINE_BLOWER_PIN, HIGH);
     winning_check();
     TimerManager::instance().update();
 }
