b50cbfa0fc97843b4b4ccfa0027670cd0234e0dd
yuvalkedar <yuvalkedar4@gmail.com>
2020-06-02 10:25:54 +0300

    Added random code generator

--- src/main.cpp
+++ src/main.cpp
@@ -46,8 +46,40 @@ DP, G, F, E, D, C, B, A
 #define CLK_PIN (10)
 #define DATA_PIN (11)
 #define LATCH_PIN (13)
+
+
 uint8_t char_array[10] = {123, 96, 93, 117, 102, 55, 63, 97, 127, 119};   // without characters
+uint8_t segment[7] = {0b01000000, 0b00000001, 0b00000010, 0b00000100, 0b00100000, 0b00010000, 0b00001000};
 // uint8_t char_array[16] = {123, 96, 93, 117, 102, 55, 63, 97, 127, 119, 111, 62, 27, 124, 31, 15};   // with characters
+long rand_digit_1;
+long rand_digit_2;
+long rand_digit_3;
+long rand_digit_4;
+
+void generate_code() {
+    for (uint8_t d = 60; d > 0; d -= 5) {
+        for (uint8_t i = 0; i < 8; i++) {
+            digitalWrite(LATCH_PIN,LOW);
+            shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,segment[i]);
+            shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,segment[i]);
+            shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,segment[i]);
+            shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,segment[i]);
+            digitalWrite(LATCH_PIN,HIGH);
+            delay(d);  
+        }
+    }
+    rand_digit_1 = random(10);
+    rand_digit_2 = random(10);
+    rand_digit_3 = random(10);
+    rand_digit_4 = random(10);
+    digitalWrite(LATCH_PIN,LOW);
+    shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_1]);
+    shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_2]);
+    shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_3]);
+    shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_4]);
+    digitalWrite(LATCH_PIN,HIGH);
+    delay(3000);
+}
 
 void setup() {
     Serial.begin(115200);
@@ -58,14 +90,10 @@ void setup() {
     digitalWrite(DATA_PIN,LOW);
     digitalWrite(LATCH_PIN,LOW);
     digitalWrite(CLK_PIN,LOW);
+
+    randomSeed(analogRead(0));
 }
 
 void loop() {
-    digitalWrite(LATCH_PIN,LOW);
-    shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[6]);
-    shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[9]);
-    shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[9]);
-    shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[1]);
-    digitalWrite(LATCH_PIN,HIGH);
-    delay(5);
+    generate_code();
 }
