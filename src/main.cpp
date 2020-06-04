5ee93882c8fb5f7e983988a616542b7886568c0c
yuvalkedar <yuvalkedar4@gmail.com>
2020-06-04 11:06:41 +0300

    fixed readings and btns

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 /*
-	Author: Yuval Kedar - KD Technology
+	Safe Machine for Gigantic - Clawee.
+    Hit the correct code or get caught - then the code will regenerate itself.
+
+    ========================================================================
+
+	Copyright (C) 2020 Yuval Kedar - KD Tech
 	Instagram: https://www.instagram.com/kd_technology/
 	Date: Jun 20
 	Dev board: Arduino Uno
-	
-	Safe Machine for Gigantic - Clawee.
-    Hit the right code or get caught - then the code will regenerate itself.
-	
-	Arduino Uno communicates with RPi.
 
         A
     ----------
@@ -52,6 +52,8 @@ BTNS KEYBOARD:
 */
 
 #include <Arduino.h>
+#include <Button.h>  // https://github.com/madleech/Button
+
 
 // #define DEBUG
 
@@ -68,24 +70,43 @@ BTNS KEYBOARD:
 #define CLK_PIN (12)
 #define LATCH_PIN (13)
 
+Button btn_1(BTN_1_PIN);
+Button btn_2(BTN_2_PIN);
+Button btn_3(BTN_3_PIN);
+Button btn_4(BTN_4_PIN);
+Button btn_5(BTN_5_PIN);
+Button btn_6(BTN_6_PIN);
+Button btn_7(BTN_7_PIN);
+Button btn_8(BTN_8_PIN);
+Button btn_9(BTN_9_PIN);
 
-uint8_t char_array[9] = {96, 93, 117, 102, 55, 63, 97, 127, 119};   // without characters and zero (123)
+
+uint8_t char_array[9] = {96, 93, 117, 102, 55, 63, 97, 127, 119};   // without characters and zero (= 123)
+uint16_t num_array[9] = {1, 2, 4, 8, 16, 32, 64, 128, 256};
 uint8_t segment[7] = {0b01000000, 0b00000001, 0b00000010, 0b00000100, 0b00100000, 0b00010000, 0b00001000};
 uint8_t btns_pins[9] = {BTN_1_PIN ,BTN_2_PIN ,BTN_3_PIN ,BTN_4_PIN ,BTN_5_PIN ,BTN_6_PIN ,BTN_7_PIN ,BTN_8_PIN ,BTN_9_PIN};
 // uint8_t char_array[16] = {123, 96, 93, 117, 102, 55, 63, 97, 127, 119, 111, 62, 27, 124, 31, 15};   // with characters
-long rand_digit_1;
-long rand_digit_2;
-long rand_digit_3;
 long rand_digit_4;
+long rand_digit_3;
+long rand_digit_2;
+long rand_digit_1;
 char ser_input;
+
+enum State {
+	DIGIT_1 = 1,
+	DIGIT_2 = 2,
+	DIGIT_3 = 3,
+	DIGIT_4 = 4
+};
+uint8_t state = DIGIT_1;
 
 uint16_t get_btns_state() {
     uint16_t btns_mask = 0;
     for (uint16_t i = 0; i < 9; i++) {
         btns_mask |= (uint16_t)(digitalRead(btns_pins[i]) << i);
     }
-    
-    return btns_mask;
+    btns_mask = ~btns_mask - 65024;
+    return btns_mask;   // Serial prints: 511 = no press, 510 = 1, 509 = 2, 507 = 3, 503 = 4, 495 = 5, 479 = 6, 447 = 7, 383 = 8, 255 = 9
 }
 
 void generate_code() {
@@ -100,40 +121,39 @@ void generate_code() {
             delay(d);  
         }
     }
-    rand_digit_1 = random(0,9);
-    rand_digit_2 = random(0,9);
-    rand_digit_3 = random(0,9);
     rand_digit_4 = random(0,9);
+    rand_digit_3 = random(0,9);
+    rand_digit_2 = random(0,9);
+    rand_digit_1 = random(0,9);
     digitalWrite(LATCH_PIN,LOW);
-    shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_1]);
-    shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_2]);
-    shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_3]);
     shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_4]);
+    shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_3]);
+    shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_2]);
+    shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_1]);
     digitalWrite(LATCH_PIN,HIGH);
-    delay(3000);
 }
 
 void delete_digit(uint8_t digit) {
     switch (digit) {
         case 1:
             digitalWrite(LATCH_PIN,LOW);
-            shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_1]);
-            shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_2]);
+            shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_4]);
             shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_3]);
+            shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_2]);
             shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,0);
             digitalWrite(LATCH_PIN,HIGH);
             break;
         case 2:
             digitalWrite(LATCH_PIN,LOW);
-            shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_1]);
-            shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_2]);
+            shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_4]);
+            shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_3]);
             shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,0);
             shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,0);
             digitalWrite(LATCH_PIN,HIGH);
             break;
         case 3:
             digitalWrite(LATCH_PIN,LOW);
-            shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_1]);
+            shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,char_array[rand_digit_4]);
             shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,0);
             shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,0);
             shiftOut(DATA_PIN,CLK_PIN,MSBFIRST,0);
@@ -152,12 +172,49 @@ void delete_digit(uint8_t digit) {
     }
 }
 
-void update_code(uint16_t mask, long dig_1, long dig_2, long dig_3, long dig_4) { //TODO: need to know the temporary random code to compare user's input
-
+void update_code(uint16_t mask) {
+    switch(state) {
+        case DIGIT_1:
+            if (mask == num_array[rand_digit_1]) {
+                delete_digit(1);
+                delay(4000);    // Delay overcomes faulty press when two or more following numbers are identical
+                state = DIGIT_2;
+            }
+            break;
+        case DIGIT_2:
+            if (mask == num_array[rand_digit_2]) {
+                delete_digit(2);
+                delay(4000);
+                state = DIGIT_3;
+            }
+            break;
+        case DIGIT_3:
+            if (mask == num_array[rand_digit_3]) {
+                delete_digit(3);
+                delay(4000);
+                state = DIGIT_4;
+            }
+            break;
+        case DIGIT_4:
+            if (mask == num_array[rand_digit_4]) {
+                delete_digit(4);
+                state = DIGIT_1;
+            }
+            break;
+    }
 }
 
 void setup() {
     Serial.begin(115200);
+    btn_1.begin();
+    btn_2.begin();
+    btn_3.begin();
+    btn_4.begin();
+    btn_5.begin();
+    btn_6.begin();
+    btn_7.begin();
+    btn_8.begin();
+    btn_9.begin();
     pinMode(DATA_PIN, OUTPUT);  
     pinMode(LATCH_PIN, OUTPUT);
     pinMode (CLK_PIN, OUTPUT);
@@ -178,6 +235,8 @@ void setup() {
     "\n"
     "     Made by KD Technology      \n"
     "\n"));
+
+    generate_code();
 }
 
 void loop() {
@@ -195,10 +254,6 @@ void loop() {
 #else
     ser_input = Serial.read();
     if (ser_input == 'g') generate_code();
-    if (ser_input == 'a') delete_digit(1);
-    if (ser_input == 's') delete_digit(2);
-    if (ser_input == 'd') delete_digit(3);
-    if (ser_input == 'f') delete_digit(4);
-    // update_code(get_btns_state());
+    update_code(get_btns_state());
 #endif
 }
