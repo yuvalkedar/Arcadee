68b0234249e78eb32dbe31e84fafecaaadf1d21d
yuvalkedar <yuvalkedar4@gmail.com>
2020-10-07 17:48:16 +0300

    minor fixes

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# Arcadee
+"Gear Machine" is a progress arcade machine for Gigantic.
+  
+Arduino Uno communicates with RPi.
+A hamer (instead of a claw) hits buttons.
+There are two button types: Red and Blue.
+Red btn = -1
+Blue btn = +1
 
-NerfGun Machine For Gigantic LTD.
+On the machine's background there is an LED matrix which shows the user's progress.
+There are 4 levels (square frames) until one reaches the core and wins.
+
+The trick? The floor, which includes the buttons, is spinning.
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,8 @@
 /*
-  Author: Yuval Kedar - KD Technology
-  Instagram: https://www.instagram.com/kd_technology/
-  Date: Oct 19
-  Dev board: Arduino Uno
-  
-  "Gear Machine" is a progress arcade machine for Gigantic.
-  
-  Arduino Uno communicates with RPi.
-  A hamer (instead of a claw) hits buttons.
-    There are two button types: Red and Blue.
-    Red btn = -1
-    Blue btn = +1
-
-    On the machine's background there is an LED matrix which shows the user's progress.
-    There are 4 levels (square frames) until one reaches the core and wins.
-
-    The trick? The floor, which includes the buttons, is spinning.
+* Author: Yuval Kedar - KD Technology
+* Instagram: https://www.instagram.com/kd_technology/
+* Date: Oct 19
+* Dev board: Arduino Uno
 */
 
 #include <FastLED.h>
@@ -24,17 +11,17 @@
 #include <timer.h>  // https://github.com/brunocalou/Timer
 #include <timerManager.h>
 
-#define WINNING_SENSOR_PIN (12)
-#define LED_DATA_PIN (6)
-#define BLUE_BTN_PIN (A0)
-#define RED_BTN_PIN (A3)
+#define WINNING_SENSOR_PIN  (12)
+#define LED_DATA_PIN        (6)
+#define BLUE_BTN_PIN        (A0)
+#define RED_BTN_PIN         (A3)
 
-#define SERIAL_BAUDRATE (115200)
-#define NUM_LEDS (64)
-#define LED_BRIGHTNESS (200)
-#define WINNING_FX_TIME (1000)  //NOTICE: make sure the number isn't too big. User might start a new game before the effect ends.
-#define DELETE_TIME_MS  (1000)
-#define DEBOUNCE_MS (50)
+#define SERIAL_BAUDRATE     (115200)
+#define NUM_LEDS            (64)
+#define LED_BRIGHTNESS      (200)
+#define WINNING_FX_TIME     (1000)
+#define DELETE_TIME_MS      (1000)
+#define DEBOUNCE_MS         (50)
 
 ezButton blue_btn(BLUE_BTN_PIN);
 ezButton red_btn(RED_BTN_PIN);
@@ -43,11 +30,12 @@ Timer delete_level;
 
 uint8_t score = 0;
 uint8_t last_score = 0;
-uint8_t level[] = {0, 28, 48, 60, 63};  //levels 0 to 4 //TODO: check I'm not exceeding 64 LEDs - it may need to be 63.
+uint8_t level[] = {0, 28, 48, 60, 63};
 uint8_t start_point = 0;
 uint32_t cur_time;
 
-void level_up(uint8_t led_num) {
+void level_up(uint8_t led_num)
+{
     if (led_num == level[1]) start_point = 0;   //up from level 0 to 1
     if (led_num == level[2]) start_point = 28;  //up from level 1 to 2
     if (led_num == level[3]) start_point = 48;  //up from level 2 to 3
@@ -57,7 +45,9 @@ void level_up(uint8_t led_num) {
     FastLED.show();
 }
 
-void level_down(uint8_t led_num) {  //clear prev level's frame and do the opposite direction effect with red color
+// Clear previous level frame & do the opposite direction effect with red color
+void level_down(uint8_t led_num)
+{
     if (led_num == level[0]) start_point = 28;  //down from level 1 to 0
     if (led_num == level[1]) start_point = 48;  //down from level 2 to 1
     if (led_num == level[2]) start_point = 60;  //down from level 3 to 2
@@ -68,7 +58,8 @@ void level_down(uint8_t led_num) {  //clear prev level's frame and do the opposi
     delete_level.start();
 }
 
-void delete_level_cb() {
+void delete_level_cb()
+{
     uint8_t led_num;
     if (start_point == 28) led_num = level[0];
     if (start_point == 48) led_num = level[1];
@@ -79,38 +70,44 @@ void delete_level_cb() {
     FastLED.show();
 }
 
-void fadeall() {
+void fade_all()
+{
     for (uint8_t i = 0; i < NUM_LEDS; i++) leds[i].nscale8(250);
 }
 
-void winning() {
+void winning()
+{
     static uint8_t hue = 0;
     for (uint8_t x = 0; x < 5; x++) {
         for (int8_t i = 0; i < NUM_LEDS; i++) {
             leds[i] = CHSV(hue++, 255, 255);
             FastLED.show(); 
-            fadeall();
+            fade_all();
         }
         for (int8_t i = (NUM_LEDS)-1; i >= 0; i--) {
             leds[i] = CHSV(hue++, 255, 255);
             FastLED.show();
-            fadeall();
+            fade_all();
         }
     }
 }
 
-void reset_game() {
+void reset_game()
+{
     score = 0;
     last_score = 4;
     digitalWrite(WINNING_SENSOR_PIN, LOW);
     FastLED.clear();
-    FastLED.show();}
+    FastLED.show();
+}
 
-void winning_check() {
+void winning_check()
+{
     (score == 4) ? analogWrite(WINNING_SENSOR_PIN, 175) : digitalWrite(WINNING_SENSOR_PIN, LOW);
 }
 
-void update_score() {
+void update_score()
+{
     if (blue_btn.isPressed()) {
         Serial.println("+PLUS+");
         if (score++ >= 4) score = 4;
@@ -152,7 +149,8 @@ void update_score() {
 }
 
 
-void setup() {
+void setup()
+{
     Serial.begin(SERIAL_BAUDRATE);
 
     pinMode(WINNING_SENSOR_PIN, OUTPUT);
@@ -180,10 +178,10 @@ void setup() {
         "\n"));
 }
 
-void loop() {
+void loop()
+{
     blue_btn.loop();
     red_btn.loop();
-    // Serial.println(score);
 
     update_score();
     TimerManager::instance().update();
